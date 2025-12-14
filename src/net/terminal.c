f992b9dd0ba9634e095070cb035db2bfd1646297
pi <3477669308@qq.com>
2025-12-14 23:04:31 +0800

    https failed.

--- src/net/terminal.c
+++ src/net/terminal.c
@@ -1,11 +1,15 @@
 #include <zephyr/shell/shell.h>
 
 extern int example_sntp_request(const struct shell *sh, size_t argc, char *argv[]);
+extern int example_https_request(const struct shell *sh, size_t argc, char *argv[]);
 
 SHELL_STATIC_SUBCMD_SET_CREATE(protocol_commands,
 	SHELL_CMD(sntp, NULL,
 		"sntp request",
 		example_sntp_request),
+	SHELL_CMD(https, NULL,
+		"https request",
+		example_https_request),
 	SHELL_SUBCMD_SET_END
 );
 
